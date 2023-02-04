38218a6e5382eeabac0885e8e6967571e01f35ff
MuhammadRizwan051 <memonrizwan051@gmail.com>
2023-02-04 18:17:27 +0500

    Changings

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,28 @@
+import { Card, CardContent, Grid, Stack, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
 
 function Home() {
   return (
-    <Box sx={{ ml: { sm: '240px' }, backgroundColor: 'lightgrey' }}>
-      Home
-    </Box>
+    <>
+      <div style={{ minHeight: `calc(100vh - 70px)`, backgroundColor: 'lightgrey' }}>
+        <Box sx={{ ml: { sm: '240px' }, }}>
+          <Grid container>
+            <Grid item xl={4}>
+              <Stack spacing={2} direction='row' sx={{ bgcolor: 'white' }}>
+                <Card>
+                  <CardContent>
+                    <Typography gutterBottom variant='h5' component='div' sx={{color:'#ffffff'}}>
+                      $500.00
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+    </>
   )
 }
 
